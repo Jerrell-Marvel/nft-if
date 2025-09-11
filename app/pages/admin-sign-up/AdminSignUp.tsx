c91af9352c0de3f472b858830c3d083b5c0dabfa
Jerrell Marvel <jerrell.marvel88@gmail.com>
2025-09-11 13:48:00 +0700

    create admin sign up page

---
+++ app/pages/admin-sign-up/AdminSignUp.tsx
@@ -0,0 +1,45 @@
+import { useNavigate } from "react-router";
+
+export default function AdminSignUpPage() {
+  let navigate = useNavigate();
+  return (
+    <div>
+      <h1>Admin sign up</h1>
+
+      <label htmlFor="email">email</label>
+      <input
+        type="text"
+        id="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        type="password"
+        id="password"
+      />
+      <label htmlFor="confirm-password">confirm password</label>
+      <input
+        type="password"
+        id="confirm-password"
+      />
+
+      <label htmlFor="profile-picture">profile picture</label>
+      <input
+        type="file"
+        id="profile-picture"
+      />
+      <label htmlFor="banner-picture">banner</label>
+      <input
+        type="file"
+        id="banner-picture"
+      />
+
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Create account
+      </button>
+    </div>
+  );
+}
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,9 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("pages/home/Home.tsx"), route("/about", "pages/about/about.tsx"), route("/sign-up", "pages/user-sign-up/UserSignUp.tsx"), route("/sign-in", "pages/user-sign-in/UserSignIn.tsx")] satisfies RouteConfig;
+export default [
+  index("pages/home/Home.tsx"),
+  route("/about", "pages/about/about.tsx"),
+  route("/sign-up", "pages/user-sign-up/UserSignUp.tsx"),
+  route("/sign-in", "pages/user-sign-in/UserSignIn.tsx"),
+  route("/admin-sign-up", "pages/admin-sign-up/AdminSignUp.tsx"),
+] satisfies RouteConfig;
