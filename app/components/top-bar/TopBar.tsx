99f2b1c2376a268bff6b6ec4745e6a5ce5a2b2b8
Jerrell Marvel <jerrell.marvel88@gmail.com>
2025-09-14 21:09:13 +0700

    create top bar component

---
+++ app/components/top-bar/TopBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function TopBar() {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="search"
+      />
+
+      <button>Cart</button>
+      <button>Avatar</button>
+    </div>
+  );
+}
