ce48631e56eec0c28aba1a50ac858609cebf5a2d
Yohan Leonardo <yohanleonardo150@gmail.com>
2025-09-21 21:32:09 +0700

    styling profile page

--- app/pages/profile/Profile.tsx
+++ app/pages/profile/Profile.tsx
@@ -1,19 +1,43 @@
-import "./profile.scss";
+import { useNavigate } from 'react-router'
+import './profile.scss'
 
 export default function Profile() {
+  let navigate = useNavigate()
+  let walletAddress = '0xe123123131'
+
   return (
     <main className="profile-page nav-padding">
-      <p>Profile</p>
+      <div className="main-container">
+        <h1>Profile</h1>
 
-      <img src="/1.jpg" />
+        <img className="profile-avatar" src="/1.jpg" alt="Profile Avatar" />
 
-      <p>Wallet address : 0xe123123131</p>
-      <button>copy</button>
+        <div className="wallet-info">
+          <p className="wallet-address">Wallet address: {walletAddress}</p>
+          <button
+            className="copy-icon"
+            onClick={() => {
+              navigator.clipboard.writeText(walletAddress)
+            }}
+          >
+            <img src="/copy-icon.png" alt="Copy Icon" />
+          </button>
+        </div>
 
-      <button>Connect wallet</button>
-      <button>Preferences</button>
-      <button>My account</button>
-      <button>Logout</button>
+        <div className="profile-actions">
+          <button className="btn">Connect Wallet</button>
+          <button className="btn">Preferences</button>
+          <button className="btn">My account</button>
+          <button
+            onClick={() => {
+              navigate('/sign-in')
+            }}
+            className="btn"
+          >
+            Logout
+          </button>
+        </div>
+      </div>
     </main>
-  );
+  )
 }
--- app/pages/user-sign-in/UserSignIn.tsx
+++ app/pages/user-sign-in/UserSignIn.tsx
@@ -1,39 +1,43 @@
-import { Link, useNavigate } from "react-router";
-import "./UserSignIn.scss";
+import { Link, useNavigate } from 'react-router'
+import './UserSignIn.scss'
 
 export default function UserSignInPage() {
-  let navigate = useNavigate();
+  let navigate = useNavigate()
   return (
     <div className="user-sign-in-page nav-padding">
       <div className="main-container">
-        <h1>User login</h1>
+        <h1 className="gradient-text">Login</h1>
+
+        <div className="gradient-line"></div>
 
         <div className="input-item">
-          <label htmlFor="email">email</label>
-          <input
-            type="text"
-            id="email"
-          />
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" />
         </div>
 
         <div className="input-item">
-          <label htmlFor="password">password</label>
-          <input
-            type="password"
-            id="password"
-          />
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
         </div>
 
-        <Link to="/">Reset password</Link>
-        <Link to="/sign-up">Sign up</Link>
+        <Link to="/reset-password" className="reset-password gradient-text">
+          Forget password?
+        </Link>
         <button
           onClick={() => {
-            navigate("/");
+            navigate('/')
           }}
+          className="btn"
         >
           Sign in
         </button>
+        <span className="sign-up-wrapper">
+          Don't have an account?{' '}
+          <Link to="/sign-up" className="gradient-text sign-up">
+            Sign up
+          </Link>
+        </span>
       </div>
     </div>
-  );
+  )
 }
