b2fde7f03f6c77e721314b85919ddbeb204e76bc
Josua <josua312@gmail.com>
2025-10-09 19:53:45 +0700

    adding filter icon

--- app/pages/home/home.tsx
+++ app/pages/home/home.tsx
@@ -6,9 +6,13 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "./home.scss";
+import Filter from "~/components/icons/filter/Filter";
 
 export function meta({}: Route.MetaArgs) {
-  return [{ title: "New React Router App" }, { name: "description", content: "Welcome to React Router!" }];
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
 }
 
 const imgUrls = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
@@ -52,7 +56,15 @@ export default function Home() {
           type: "fraction",
           // Custom render function for the fraction
           renderFraction: function (currentClass, totalClass) {
-            return '<span class="' + currentClass + '"></span>' + " of " + '<span class="' + totalClass + '"></span>';
+            return (
+              '<span class="' +
+              currentClass +
+              '"></span>' +
+              " of " +
+              '<span class="' +
+              totalClass +
+              '"></span>'
+            );
           },
         }}
         // This is important: it re-initializes Swiper after the refs are mounted
@@ -69,127 +81,141 @@ export default function Home() {
         <SwiperSlide>Slide 1</SwiperSlide>
       </Swiper>
       <main className="home-page main-container nav-padding">
-        <div className="left-container">
-          <div>
-            <h1 className="gradient-text">Mountain Cliffs NFT</h1>
+        <div className="content">
+          <div className="left-container">
+            <div>
+              <h1 className="gradient-text">Mountain Cliffs NFT</h1>
 
-            <div className="nft-desc-container">
-              <img
-                src={selectedImage}
-                className="img-item"
-              />
+              <div className="nft-desc-container">
+                <img src={selectedImage} className="img-item" />
 
-              <p className="gradient-text">
-                A dramatic coastal scene where towering white cliffs meet the restless ocean. The cliffs rise steeply from the narrow strip of beach, their faces streaked with earthy tones and patches of greenery. breaking into white foam
-                as waves lap onto the sand. The coastline curves gracefully into the distance, giving a sense of both grandeur and solitude, as if this rugged place belongs more to nature than to people.
-              </p>
+                <p className="gradient-text">
+                  A dramatic coastal scene where towering white cliffs meet the
+                  restless ocean. The cliffs rise steeply from the narrow strip
+                  of beach, their faces streaked with earthy tones and patches
+                  of greenery. breaking into white foam as waves lap onto the
+                  sand. The coastline curves gracefully into the distance,
+                  giving a sense of both grandeur and solitude, as if this
+                  rugged place belongs more to nature than to people.
+                </p>
+              </div>
             </div>
+
+            <Link to="/purchase/1123123">
+              <button className="btn">Detail</button>
+            </Link>
           </div>
 
-          <Link to="/purchase/1123123">
-            <button className="btn">Detail</button>
-          </Link>
-        </div>
+          <div className="right-container">
+            <h1 className="gradient-text">Check our NFT product</h1>
 
-        <div className="right-container">
-          <h1 className="gradient-text">Check our NFT product</h1>
-
-          <div className="type-container">
-            <p className="gradient-text">Display : {getActiveDisplay()}</p>
-            <button
-              className="btn"
-              onClick={changeDisplay}
-            >
-              {getActiveDisplay() === "Single" ? "Collections" : "Single"}
-            </button>
-          </div>
-
-          <Swiper
-            // Pass modules
-            modules={[Navigation, Pagination]}
-            className="mySwiper"
-            // Configure Navigation
-            navigation={{
-              prevEl: navigationPrevRef.current,
-              nextEl: navigationNextRef.current,
-            }}
-            // Configure Pagination
-            pagination={{
-              el: paginationRef.current,
-              type: "fraction",
-              // Custom render function for the fraction
-              renderFraction: function (currentClass, totalClass) {
-                return '<span class="' + currentClass + '"></span>' + " of " + '<span class="' + totalClass + '"></span>';
-              },
-            }}
-            // This is important: it re-initializes Swiper after the refs are mounted
-            onBeforeInit={(swiper) => {
-              //@ts-ignore
-              swiper.params.navigation.prevEl = navigationPrevRef.current;
-              //@ts-ignore
-              swiper.params.navigation.nextEl = navigationNextRef.current;
-              //@ts-ignore
-              swiper.params.pagination.el = paginationRef.current;
-            }}
-          >
-            {Array.from({
-              length: 10,
-            }).map(() => {
-              return (
-                <SwiperSlide>
-                  <div
-                    className="img-container"
-                    style={{
-                      backgroundColor: getActiveDisplay() === "Collection" ? "salmon" : "blue",
-                    }}
-                  >
-                    {imgUrls.map((imgUrl) => {
-                      return (
-                        <img
-                          className="img-item"
-                          src={imgUrl}
-                          key={imgUrl}
-                          onClick={() => {
-                            setSelectedImage(imgUrl);
-                          }}
-                        />
-                      );
-                    })}
-
-                    {imgUrls.map((imgUrl) => {
-                      return (
-                        <img
-                          className="img-item"
-                          src={imgUrl}
-                          key={imgUrl}
-                          onClick={() => {
-                            setSelectedImage(imgUrl);
-                          }}
-                        />
-                      );
-                    })}
-                  </div>
-                </SwiperSlide>
-              );
-            })}
-          </Swiper>
-
-          <div className="custom-controls-container">
-            <div
-              ref={navigationPrevRef}
-              className="custom-button custom-button-prev"
-            >
-              left
+            <div className="type-container">
+              <p className="gradient-text">Display : {getActiveDisplay()}</p>
+              <button className="btn" onClick={changeDisplay}>
+                {getActiveDisplay() === "Single" ? "Collections" : "Single"}
+              </button>
             </div>
-            <div
-              ref={paginationRef}
-              className="custom-pagination"
-            ></div>
-            <div
-              ref={navigationNextRef}
-              className="custom-button custom-button-next"
+
+            <div className="filter-container">
+              <span>Showing result of "..."</span>
+              <Filter />
+            </div>
+
+            <Swiper
+              // Pass modules
+              modules={[Navigation, Pagination]}
+              className="mySwiper"
+              // Configure Navigation
+              navigation={{
+                prevEl: navigationPrevRef.current,
+                nextEl: navigationNextRef.current,
+              }}
+              // Configure Pagination
+              pagination={{
+                el: paginationRef.current,
+                type: "fraction",
+                // Custom render function for the fraction
+                renderFraction: function (currentClass, totalClass) {
+                  return (
+                    '<span class="' +
+                    currentClass +
+                    '"></span>' +
+                    " of " +
+                    '<span class="' +
+                    totalClass +
+                    '"></span>'
+                  );
+                },
+              }}
+              // This is important: it re-initializes Swiper after the refs are mounted
+              onBeforeInit={(swiper) => {
+                //@ts-ignore
+                swiper.params.navigation.prevEl = navigationPrevRef.current;
+                //@ts-ignore
+                swiper.params.navigation.nextEl = navigationNextRef.current;
+                //@ts-ignore
+                swiper.params.pagination.el = paginationRef.current;
+              }}
             >
-              right
+              {Array.from({
+                length: 10,
+              }).map(() => {
+                return (
+                  <SwiperSlide>
+                    <div
+                      className="img-container"
+                      style={{
+                        backgroundColor:
+                          getActiveDisplay() === "Collection"
+                            ? "salmon"
+                            : "blue",
+                      }}
+                    >
+                      {imgUrls.map((imgUrl) => {
+                        return (
+                          <img
+                            className="img-item"
+                            src={imgUrl}
+                            key={imgUrl}
+                            onClick={() => {
+                              setSelectedImage(imgUrl);
+                            }}
+                          />
+                        );
+                      })}
+
+                      {imgUrls.map((imgUrl) => {
+                        return (
+                          <img
+                            className="img-item"
+                            src={imgUrl}
+                            key={imgUrl}
+                            onClick={() => {
+                              setSelectedImage(imgUrl);
+                            }}
+                          />
+                        );
+                      })}
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+
+            <div className="custom-controls-container">
+              <div
+                ref={navigationPrevRef}
+                className="custom-button custom-button-prev"
+              >
+                left
+              </div>
+              <div ref={paginationRef} className="custom-pagination"></div>
+              <div
+                ref={navigationNextRef}
+                className="custom-button custom-button-next"
+              >
+                right
+              </div>
             </div>
           </div>
         </div>
